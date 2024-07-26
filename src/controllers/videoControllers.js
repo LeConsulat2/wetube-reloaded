1f90516e4607dee03b208e2fedbdbcf41fcbb98d
LeConsulat2 <wooine2@gmail.com>
2024-07-27 07:24:35 +1200

    updateeee

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -26,16 +26,18 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
-  const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: '',
-    hashtags: hashtags.split(',').map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect('/');
+  const { title, description, hastags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hastags.split(',').map((word) => `#${word}`),
+    });
+    return res.redirect('/');
+  } catch (error) {
+    return res.render('upload', {
+      pageTitle: 'Upload Video',
+      errorMessage: error._message,
+    });
+  }
 };
