c5026ba0b2795c07426f57cba8ceafcb24769e49
LeConsulat2 <wooine2@gmail.com>
2024-07-28 20:15:35 +1200

    Edit Video part 1

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -8,13 +8,19 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  return res.render('watch', { pageTitle: `Watching`, video });
+  if (video === null) {
+    return res.render('404', { pageTitle: 'Video not found' });
+  }
+  return res.render('watch', { pageTitle: video.title, video });
 };
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  return res.render('edit', { pageTitle: `Editing`, video });
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found' });
+  }
+  return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = async (req, res) => {
