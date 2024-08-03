f6095238273a2dfac7ebfd1df27d5cf5450a0d1c
LeConsulat2 <wooine2@gmail.com>
2024-08-04 11:53:28 +1200

    #6 Mongodb and mongoose part finished

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,7 +1,7 @@
 // src/routers/globalRouter.js
 import express from 'express';
 import { join, login } from '../controllers/userControllers';
-import { home } from '../controllers/videoControllers';
+import { home, search } from '../controllers/videoControllers';
 
 const globalRouter = express.Router();
 
@@ -10,5 +10,6 @@ console.log({ join, home }); // Debugging line
 globalRouter.get('/', home);
 globalRouter.get('/join', join);
 globalRouter.get('/login', login);
+globalRouter.get('/search', search);
 
 export default globalRouter;
