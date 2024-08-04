b24bada1e63dc42a5627a605f1968f8e36b0b81b
LeConsulat2 <wooine2@gmail.com>
2024-08-04 12:12:54 +1200

    코드정리

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -73,7 +73,7 @@ export const search = async (req, res) => {
   if (keyword) {
     videos = await Video.find({
       title: {
-        $regex: new RegExp(),
+        $regex: new RegExp(keyword, 'i'),
       },
     });
   }
