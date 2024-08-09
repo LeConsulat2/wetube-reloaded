c87030e8fe0d68df9c6172bf598fcd6c67471211
LeConsulat2 <wooine2@gmail.com>
2024-08-09 17:00:48 +1200

    Update server.js and catching everyone using cookies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
-import session from "express-session";
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import globalRouter from './routers/globalRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
@@ -13,12 +14,13 @@ app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    session({
-        secret:"Hello",
-        resave:true,
-        saveUninitialized: true,
-    })
-)
+  session({
+    secret: 'Hello',
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wetube' }),
+  }),
+);
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
