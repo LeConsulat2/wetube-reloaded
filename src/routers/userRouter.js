96c4ac3e40de4d1924103cc37d587d9a0149f494
LeConsulat2 <wooine2@gmail.com>
2024-08-10 15:23:05 +1200

    7.17 Github Login Part 2 done

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,20 @@
-import express from "express";
-import { edit, remove } from "../controllers/userControllers";
+import express from 'express';
+import {
+  edit,
+  remove,
+  logout,
+  search,
+  startGithubLogin,
+  finishGithubLogin,
+} from '../controllers/userControllers';
 
 const userRouter = express.Router();
 
-userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
+userRouter.get('/logout', logout);
+userRouter.get('/edit', edit);
+userRouter.get('/delete', remove);
+userRouter.get('/github/start', startGithubLogin);
+userRouter.get('github/finish', finishGithubLogin);
+userRouter.get(':id', search);
 
 export default userRouter;
